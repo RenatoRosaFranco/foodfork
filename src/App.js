606d552c70f6e8e0fec284b62b9671895780ac11
Renato Rosa Franco <Renato_ny@live.com>
2019-05-08 11:56:25 -0300

    Adicionando paramtro nome da receita a busca

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 	getRecipe = async (e) => {
 	 	const recipeName = e.target.elements.recipeName.value;
 	 	e.preventDefault();
-	 	const  api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&page=2`);
+	 	const  api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
 		const  data = await api_call.json();
 		this.setState({recipes: data.recipes });
 		console.log(this.state.recipes);
@@ -28,7 +28,9 @@ class App extends Component {
 	    	</header>
 	    	<Form getRecipe={this.getRecipe}/>
 
-	    	
+	    	{ this.state.recipes.map((recipe) => {
+	    		return <p key={ recipe.recipe_id }>{ recipe.title } </p>
+	    	}) }	    	
 	  	</div>
 		);
 	}
